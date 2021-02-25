0e27ce643a25b685b73d762e1f5b8427bc743c02
fade437 <samyogbohara437@gmail.com>
2021-02-25 10:06:38 +0545

    area of triangle

---
+++ LabOne/area_triangle.py
@@ -0,0 +1,7 @@
+#Write a program that reads the length of the base and height of a right angled triangle and prints the area.
+#Every number is given on a separate line.
+
+base  =int(input("Enter base:"))
+height  =int(input("Enter height:"))
+area = 1/2*base*height
+print(f"The area of right angled triangle is{area}")
