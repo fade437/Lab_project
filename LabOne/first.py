5035bcd52f41a07f3b7d62029c53dafba92b5f5f
fade437 <samyogbohara437@gmail.com>
2021-02-25 09:50:35 +0545

    added printing format

--- LabOne/first.py
+++ LabOne/first.py
@@ -4,3 +4,13 @@ num2 = int(input("Enter second number:"))
 num3 = int(input("Enter third number:"))
 sum  =num1+num2+num3
 print("The sum is ",sum)
+
+
+name = input("Enter your name:")
+age = int(input("Enter age:"))
+
+print("Hello my name is " + name + "and I am " + str(age) + " years old")
+print("Hello my name is", name, "and I am", age, "years old")
+print("Hello my name is %s and I am %d years old" % (name,age))
+print("Hello my name is {} and I am {} years old".format(name, age))
+print(f"Hello my name is {name} and I am {age} years old")
